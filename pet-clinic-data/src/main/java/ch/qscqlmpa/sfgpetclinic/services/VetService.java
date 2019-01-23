551ae1b9eb2f647693f047b1c94c029c34172ff4
Michael Heiniger <michael.heiniger@adnovum.ch>
2019-01-23 09:54:14 +0100

    Implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/ch/qscqlmpa/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package ch.qscqlmpa.sfgpetclinic.services;
+
+import ch.qscqlmpa.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
